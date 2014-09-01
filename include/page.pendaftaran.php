2f35d7b10194ca72ef2bf81522b9305cdd65e2cc
Seto El Kahfi <setoelkahfi@gmail.com>
2014-09-01 14:50:06 +0700

    Sent email page pendaftaran is now working

--- include/page.pendaftaran.php
+++ include/page.pendaftaran.php
@@ -181,6 +181,26 @@ if ($action == '') :
 				{
 					echo '<script>alert("Sorry, can not upload photo.");</script>';
 				}
+				$body_mail = '<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body>
+		<h1>Terimakasih.</h1>
+		<p>Saat ini Anda sudah terdaftar sebagai calon siswa bimbel CEC. Untuk mengaktivasi akun Anda, segera lakukan pembayaran dengan datang langsung ke bimbel CEC.</p>
+		<p>Anda sudah bisa masuk ke ruang siswa untuk melihat status pembayaran. Silahkan login menggunakan akun Anda.</p>
+		No Formulir : '.$nomor_formulir.'<br/>
+		Password: '.$tanggal_lahir.'<br/>
+		<p>Silahkan login <a href="http://bimbelcec.com/?pg=siswa&do=login">disini</a>.</p>
+		</body>
+		</html>';
+				$headers = "From: admin@bimbelcec.com\r\n";
+				$headers .= "Reply-to: admin@bimbelcec.com\r\n";
+				$headers .= "Content-type: text/html";
+				if ((isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] == 'www.bimbelcec.com' || $_SERVER['HTTP_HOST'] == 'bimbelcec.com')) || $_SERVER['SERVER_NAME'] == 'www.bimbelcec.com' || $_SERVER['SERVER_NAME'] == 'bimbelcec.com')
+				{
+					$mail_sent = mail($email, "Registrasi Online - Bimbel CEC", $body_mail, $headers);
+				}
+				else
+				{
+					echo '<script>alert("Maaf, saat ini sistem sedang offline. Sistem tidak bisa mengirim informasi login ke email Anda. Mohon cetak formulir Anda dan segera hubungi staf Bimbel CEC. Mohon maaf atas ketidaknyamanan Anda. Terimakasih.");</script><br/>';
+				}
 				// freeup $_POST variables
 				foreach($_POST as $key=>$value)
 				{
