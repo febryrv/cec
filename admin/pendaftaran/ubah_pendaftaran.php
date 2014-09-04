49808ef588bb70de72d6b22eb8ece48a2cafffcd
Seto El Kahfi <setoelkahfi@gmail.com>
2014-09-04 15:55:05 +0700

    Fix email pengaktifan akun setelah melakukan pembayaran

--- admin/pendaftaran/ubah_pendaftaran.php
+++ admin/pendaftaran/ubah_pendaftaran.php
@@ -9,16 +9,28 @@ if (isset($_POST['pembayaran']) && $_POST['pembayaran'] == 'lunas')
 		VALUES ('".$nis."', '".$tampil['nama']."', '".$tampil['foto']."', '".$tampil['kode_program']."', '".$tampil['jenis_kelamin']."', '".$tampil['agama']."', '".$tampil['email']."', '".$tampil['password']."', '".$tampil['alamat']."', '".$tampil['kota_lahir']."', '".$tampil['tanggal_lahir']."', '".$tampil['sekolah']."', '".$tampil['kelas']."', '".$tampil['hobi']."', '".$tampil['cita']."', '".$tampil['tlp']."', '".$tampil['ayah']."', '".$tampil['ibu']."', '".$tampil['pekerjaanayah']."', '".$tampil['pekerjaanibu']."', '".$tampil['tanggal_daftar']."')";
 	mysql_query($sql) or die(mysql_error());
 	mysql_query("UPDATE formulir SET lunas = '1' WHERE nomor_formulir = '".$tampil['nomor_formulir']."'");
-	?>
-	<script>
-	alert('Data pendaftar berhasil diubah.');
-	document.location = '?pg=pendaftaran/data_pendaftar';
-	
-	</script>	
-	<?php
-	//echo $nis;
+	$body_mail = '<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body>
+		<h1>Terima kasih.</h1>
+		<p>Saat ini anda sudah menjadi siswa di "BIMBEL CEL"</p>
+		<p>Segera lakukan login di ruang siswa untuk melihat jadwal.</p>
+		<br/>
+		<p>NIS : '.$nis.'</p>
+		<p>Passowrd: '.$tampil['tanggal_lahir'].'</p> 
+		<br>
+		</body></html>';
+	$headers = "From: admin@bimbelcec.com\r\n";
+	$headers .= "Reply-to: admin@bimbelcec.com\r\n";
+	$headers .= "Content-type: text/html";
+	if ((isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] == 'www.bimbelcec.com' || $_SERVER['HTTP_HOST'] == 'bimbelcec.com')) || $_SERVER['SERVER_NAME'] == 'www.bimbelcec.com' || $_SERVER['SERVER_NAME'] == 'bimbelcec.com')
+	{
+		$mail_sent = mail($tampil['email'], "Aktivasi Akun - Bimbel CEC", $body_mail, $headers);
+		echo '<script>alert("Email terkirim.");location="index.php?pg=pendaftaran/data_kontak";</script>';
+	}
+	else
+	{
+		echo '<script>alert("Maaf, saat ini sistem sedang offline..");location="index.php?pg=kontak/data_kontak";</script>';
+	}
 }
-
 ?>
 
 <form action="" method="post" enctype="multipart/form-data" name="form1">
